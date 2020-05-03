51e15d866e125a3402b0c1b960e25aad14ae8189
Karles <karles@karlesnine.com>
2020-05-03 15:40:49 +0200

    [github] init Action workflow

--- run.sh
+++ run.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-echo "Enter the path of your ansible folder, followed by [ENTER]:"
-read ansiblePath
-echo "Enter the path of your terraform folder, followed by [ENTER]:"
-read terraformPath
 
-echo "Enter your public ssh key (optionnal) (to override root key), followed by [ENTER]:"
-read sshPubKey
-
-echo "Enter the listen port (default 8001), followed by [ENTER]:"
-read listenPort
+source ./run.init
 
 if [ -z $listenPort ]
 then
@@ -16,29 +8,27 @@ then
 fi
 
 if [ "$(uname)" == "Darwin" ]; then
-  docker run --hostname=dev --dns=192.168.136.7 \
+  docker container run --hostname=DevK9 \
 	-v $ansiblePath:/etc/ansible:delegated \
 	-v $terraformPath:/etc/terraform:delegated \
 	-v $HOME/.aws:/root/.aws \
-	-p $listenPort:22 -d -P --name devops-toolbox gitlab.vestiairecollective.com:4567/tools/docker-devops-toolbox:latest
+	-p $listenPort:22 -d -P --name $ContainerName $ImageName:latest
 else
-  docker run --hostname=dev --dns=192.168.136.7 \
+  docker container run --hostname=DevK9 \
 	-v $ansiblePath:/etc/ansible \
 	-v $terraformPath:/etc/terraform \
 	-v $HOME/.aws:/root/.aws \
-	-p $listenPort:22 -d -P --name devops-toolbox gitlab.vestiairecollective.com:4567/tools/docker-devops-toolbox:latest
+	-p $listenPort:22 -d -P --name $ContainerName $ImageName:latest
 fi
 	#statements
 
 
 if [ ! -z "$sshPubKey" ]
 then
-    docker exec -it devops-toolbox sh -c "echo \"$sshPubKey\" > /root/.ssh/id_rsa.pub"
-    docker exec -it devops-toolbox sh -c "echo \"$sshPubKey\" > /root/.ssh/authorized_keys"
+    docker exec -it $ContainerName sh -c "echo \"$sshPubKey\" > /root/.ssh/id_rsa.pub"
+    docker exec -it $ContainerName sh -c "echo \"$sshPubKey\" > /root/.ssh/authorized_keys"
 fi
 
-gitConfigName=$(git config --global user.name)
-gitConfigMail=$(git config --global user.email)
 if [ ! -z "$gitConfigName" ]
 then
 	docker exec -it devops-toolbox git config --global user.name $gitConfigName
